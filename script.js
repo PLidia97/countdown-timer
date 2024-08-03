9d6d40b0157041f4b0f2ee9d51619df7c37e5507
Lidia <lidia_tanase97@yahoo.com>
2024-08-03 14:21:04 +0300

    set & get functions

---
+++ script.js
@@ -0,0 +1,56 @@
+const hoursInputEl = document.getElementById("hoursInput");
+const minutesInputEl = document.getElementById("minutesInput");
+const secondsInputEl = document.getElementById("secondsInput");
+
+const hoursOutputEl = document.getElementById("hoursOutput");
+const minutesOutputEl = document.getElementById("minutesOutput");
+const secondsOutputEl = document.getElementById("secondsOutput");
+
+const startBtnEl = document.getElementById("startTimer");
+
+let targetTime;
+let timerInterval;
+
+const updateTimer = () => {
+  if (targetTime) {
+    const differenceInSeconds = Math.floor(targetTime - Date.now()) / 1000;
+    if (differenceInSeconds < 1) {
+      clearInterval(timerInterval);
+    }
+    const hours = Math.floor(differenceInSeconds / 3600);
+    const minutes = Math.floor(differenceInSeconds / 60) % 60;
+    const seconds = Math.floor(differenceInSeconds % 60);
+
+    hoursOutputEl.textContent = `${hours} hours`;
+    minutesOutputEl.textContent = `${minutes} minutes`;
+    secondsOutputEl.textContent = `${seconds} seconds`;
+  }
+};
+
+startBtnEl.addEventListener("click", () => {
+  const futureHours = parseInt(hoursInputEl.value);
+  const futureMinutes = parseInt(minutesInputEl.value);
+  const futureSeconds = parseInt(secondsInputEl.value);
+
+  const date = new Date();
+  const currentHours = date.getHours();
+  const currentMinutes = date.getMinutes();
+  const currentSeconds = date.getSeconds();
+
+  date.setHours(currentHours + futureHours);
+  date.setMinutes(currentMinutes + futureMinutes);
+  date.setSeconds(currentSeconds + futureSeconds);
+
+  targetTime = date.getTime();
+  localStorage.setItem("targetTime", targetTime);
+  updateTimer();
+  timerInterval = setInterval(updateTimer, 500);
+});
+
+const storedTargetTime = localStorage.getItem("targetTime");
+
+if (storedTargetTime) {
+  targetTime = storedTargetTime;
+  updateTimer();
+  timerInterval = setInterval(updateTimer, 500);
+}
